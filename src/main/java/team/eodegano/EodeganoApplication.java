9a0df78b30e9c3aff3550619f52684c2fc5335fb
seungchan <s24041@gsm.hs.kr>
2025-11-05 22:55:38 +0900

    init

---
+++ src/main/java/team/eodegano/EodeganoApplication.java
@@ -0,0 +1,13 @@
+package team.eodegano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodeganoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodeganoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/eodegano/EodeganoApplicationTests.java
@@ -0,0 +1,13 @@
+package team.eodegano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodeganoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
