f8b0c2bd13ec119ac84655f4ce5e87eaa72d34c8
seungchan <s24041@gsm.hs.kr>
2025-11-06 13:33:14 +0900

    add :: trip관련 엔티티 추가

---
+++ src/main/java/team/eodegano/domain/trip/entity/constant/Category.java
@@ -0,0 +1,5 @@
+package team.eodegano.domain.trip.entity.constant;
+
+public enum Category {
+    HEALING, ACTIVITY, HOTPLACE, CULTURE, NATURE, FOOD, RESORT, SHOPPING, LANDMARK
+}
---
+++ src/main/java/team/eodegano/domain/trip/entity/constant/Ride.java
@@ -0,0 +1,5 @@
+package team.eodegano.domain.trip.entity.constant;
+
+public enum Ride {
+    TRAIN, CAR
+}
